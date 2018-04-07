c225ca56a41ce9502dc3892d6d08e9a7e0acba33
Juan Hurtado <juanhurtado4@outlook.com>
2018-04-07 09:43:49 -0700

    Feat: Add fade span

--- 46mile/views/index.php
+++ 46mile/views/index.php
@@ -8,7 +8,9 @@
   <body>
     <header>
       <div class="header-text">
-        <h1 id="header-title">Need help with</h1>
+        <h1 id="header-title">
+          Need help with <span id="header-fade"></span>
+        </h1>
         <p>We help with your Marketing and Advertising needs</p>
       </div>
     </header>
