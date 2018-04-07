aef195990520bf63a1b9f56ccd6f04b8aa3ec264
Juan Hurtado <juanhurtado4@outlook.com>
2018-04-07 09:40:55 -0700

    Feat: Add text loop interval

--- 46mile/index.js
+++ 46mile/index.js
@@ -1,8 +1,20 @@
 // Document objects
-let headerTitle = document.getElementById("header-title");
+let headerFade = document.getElementById("header-fade");
 
 const featureList = [
   "Research", "Strategy", "Media planning & buying",
   "Social Media", "SEO", "PR", "Influencer Marketing",
   "Creative services", "Website Development & Design"
 ];
+
+const featureLoop = (index) => {
+  // Start loop delay
+  setInterval(() => {
+    headerFade.innerHTML = featureList[index];
+    index += 1;
+    // Start at the beginning of array
+    if (index === featureList.length) { index = 0 }
+  }, 2000)
+}
+
+featureLoop(0);
