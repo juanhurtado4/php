ff2bc2096adb169ea35787393467d389855304ce
Juan Hurtado <juanhurtado4@outlook.com>
2018-04-08 18:26:06 -0700

    Fix: Modify semantic layout

--- 46mile/views/index.php
+++ 46mile/views/index.php
@@ -17,14 +17,16 @@
       </div>
     </div>
 
-    <nav>
+    <header>
       <img class="logo" src="./media/46mileLogo_39x50.svg" alt="46mile logo" />
-      <a href="/about">About</a>
-      <a href="/services">Services</a>
-      <a href="/library">Library</a>
-      <a href="/blog">Blog</a>
-      <button type="button" name="button">We Can Help</button>
+      <nav>
+        <a href="/about">About</a>
+        <a href="/services">Services</a>
+        <a href="/library">Library</a>
+        <a href="/blog">Blog</a>
+        <button type="button" name="button">We Can Help</button>
     </nav>
+    </header>
 
     <script src="../index.js"></script>
   </body>
